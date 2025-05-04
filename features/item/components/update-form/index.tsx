6befb5945824c03f64206414e92a67408b423a73
Zeilar <philip_angelin@hotmail.com>
2025-05-04 18:34:06 +0200

    feat: move away from accordions

--- features/item/components/update-form/index.tsx
+++ features/item/components/update-form/index.tsx
@@ -3,26 +3,19 @@
 import { enqueueSnackbar } from "notistack";
 import { useAppForm } from "@/hooks";
 import { updateItem } from "./action";
-import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
-  Alert,
-  Box,
-  Button,
-  FormControl,
-  Typography,
-} from "@mui/material";
+import { Alert, Box, Button, Divider, FormControl, Paper, Typography } from "@mui/material";
 import z from "zod";
-import { useEffect } from "react";
-import { useDropzone } from "react-dropzone";
-import { ExpandMore, Upload } from "@mui/icons-material";
+import { type FileRejection, useDropzone } from "react-dropzone";
+import { Upload } from "@mui/icons-material";
 import { FilesTransferList } from "./files-transfer-list";
 
 interface Fields {
   title?: string;
   articleId?: string;
-  files: File[];
+  files: {
+    accepted: File[];
+    rejected: FileRejection[];
+  };
   filesToRemove: string[];
   quantity: number;
 }
@@ -43,15 +36,10 @@ function successSnackbar() {
 }
 
 export function UpdateItemForm({ id, title, files, articleId, quantity }: UpdateFormProps) {
-  const { getRootProps, getInputProps, isDragActive, acceptedFiles, fileRejections } = useDropzone({
-    multiple: true,
-    maxSize: 10_000_000,
-    maxFiles: 10,
-  });
   const form = useAppForm({
     defaultValues: {
       title,
-      files: [],
+      files: { accepted: [], rejected: [] },
       quantity: quantity ?? 1,
       articleId: articleId ?? "",
       filesToRemove: [],
@@ -65,18 +53,21 @@ export function UpdateItemForm({ id, title, files, articleId, quantity }: Update
           quantity,
           articleId: articleId || null,
         },
-        files,
+        files.accepted,
         filesToRemove
       );
+      form.resetField("files");
       successSnackbar();
     },
   });
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    multiple: true,
+    maxSize: 10_000_000,
+    maxFiles: 10,
+    onDrop: (accepted, rejected) => form.setFieldValue("files", { accepted, rejected }),
+  });
 
   const existingFiles = files.split(",").filter(Boolean);
-
-  useEffect(() => {
-    form.setFieldValue("files", acceptedFiles as File[]);
-  }, [acceptedFiles, form, files]);
 
   return (
     <Box
@@ -189,110 +180,148 @@ export function UpdateItemForm({ id, title, files, articleId, quantity }: Update
           </form.AppField>
           <Box width="100%" />
         </Box>
-        <FormControl>
-          <Box
-            {...getRootProps()}
-            borderRadius={1}
-            border="2px dashed"
-            borderColor={isDragActive ? "primary.main" : "grey.400"}
-            height={200}
-            display="flex"
-            sx={{ cursor: "pointer" }}
-          >
-            <Box m="auto" sx={{ pointerEvents: "none" }}>
-              <Typography
-                display="flex"
-                flexDirection="column"
-                variant="h6"
-                alignItems="center"
-                gap={0.25}
-              >
-                <Upload color="primary" />
-                Drop your files here
-              </Typography>
-              <Typography variant="subtitle2" fontWeight={400}>
-                Or click anywhere in the area
-              </Typography>
-              <Box component="input" {...getInputProps()} />
-            </Box>
-          </Box>
-          <Typography color="textSecondary" variant="caption" mt={0.75}>
-            Max 10 files, up to 10MB per file. Filenames must be unique.
-          </Typography>
-          <Accordion disabled={existingFiles.length === 0} sx={{ mt: 1.5 }}>
-            <AccordionSummary expandIcon={<ExpandMore />}>
-              Existing ({existingFiles.length})
-            </AccordionSummary>
-            <AccordionDetails>
-              <FilesTransferList
-                initial={existingFiles}
-                onChange={(value) => form.setFieldValue("filesToRemove", value)}
-              />
-            </AccordionDetails>
-          </Accordion>
-          <Accordion disabled={acceptedFiles.length === 0} sx={{ mt: 1.5 }}>
-            <AccordionSummary expandIcon={<ExpandMore />}>
-              Accepted ({acceptedFiles.length})
-            </AccordionSummary>
-            <AccordionDetails
-              sx={{
-                maxHeight: 100,
-                overflow: "auto",
-                display: "flex",
-                flexDirection: "column",
-                gap: 0.75,
-              }}
-            >
-              {acceptedFiles.map((acceptedFile, i) => (
-                <Alert
-                  key={i}
-                  variant="outlined"
-                  severity="success"
-                  sx={{ mr: "2px", py: 0, px: 1 }}
+        <form.AppField name="files">
+          {(field) => {
+            const { accepted, rejected } = field.state.value;
+
+            return (
+              <FormControl>
+                <Box
+                  {...getRootProps()}
+                  borderRadius={1}
+                  border="2px dashed"
+                  borderColor={isDragActive ? "primary.main" : "grey.400"}
+                  height={200}
+                  display="flex"
+                  sx={{ cursor: "pointer" }}
                 >
-                  <Typography
-                    whiteSpace="nowrap"
-                    overflow="hidden"
-                    textOverflow="ellipsis"
-                    variant="subtitle2"
+                  <Box
+                    m="auto"
+                    display="flex"
+                    flexDirection="column"
+                    textAlign="center"
+                    sx={{ pointerEvents: "none" }}
                   >
-                    {acceptedFile.name}
-                  </Typography>
-                </Alert>
-              ))}
-            </AccordionDetails>
-          </Accordion>
-          <Accordion disabled={fileRejections.length === 0} sx={{ mt: 1.5 }}>
-            <AccordionSummary expandIcon={<ExpandMore />}>
-              Accepted ({fileRejections.length})
-            </AccordionSummary>
-            <AccordionDetails
-              sx={{
-                maxHeight: 100,
-                overflow: "auto",
-                display: "flex",
-                flexDirection: "column",
-                gap: 0.75,
-              }}
-            >
-              {fileRejections.map((fileRejection, i) => (
-                <Alert key={i} variant="outlined" severity="error" sx={{ mr: "2px", py: 0, px: 1 }}>
-                  <Typography
-                    whiteSpace="nowrap"
-                    overflow="hidden"
-                    textOverflow="ellipsis"
-                    variant="subtitle2"
-                  >
-                    {fileRejection.file.name}
-                  </Typography>
-                  <Typography variant="subtitle2" fontWeight={400}>
-                    {fileRejection.errors.at(0)?.message}
-                  </Typography>
-                </Alert>
-              ))}
-            </AccordionDetails>
-          </Accordion>
-        </FormControl>
+                    <Typography
+                      display="flex"
+                      flexDirection="column"
+                      variant="h6"
+                      alignItems="center"
+                      gap={0.25}
+                    >
+                      <Upload color="primary" />
+                      Drop your files here
+                    </Typography>
+                    <Typography variant="subtitle2" fontWeight={400}>
+                      Or click anywhere in the area
+                    </Typography>
+                    <Typography color="textSecondary" variant="caption" mt={1.5}>
+                      Max 10 files, up to 10MB per file. Filenames must be unique.
+                    </Typography>
+                    <input {...getInputProps()} />
+                  </Box>
+                </Box>
+                <Box display="flex" flexDirection="column" gap={0.75}>
+                  <Paper sx={{ p: 1.5, display: "flex", flexDirection: "column", gap: 1.5 }}>
+                    <Typography
+                      variant="subtitle2"
+                      color={existingFiles.length === 0 ? "textDisabled" : undefined}
+                    >
+                      Existing ({existingFiles.length})
+                    </Typography>
+                    <FilesTransferList
+                      initial={existingFiles}
+                      onChange={(value) => form.setFieldValue("filesToRemove", value)}
+                    />
+                  </Paper>
+                  <Paper>
+                    <Typography
+                      variant="subtitle2"
+                      p={1.5}
+                      color={accepted.length === 0 ? "textDisabled" : undefined}
+                    >
+                      Accepted ({accepted.length})
+                    </Typography>
+                    {accepted.length > 0 ? (
+                      <>
+                        <Divider />
+                        <Box
+                          display="flex"
+                          flexDirection="column"
+                          gap={1.5}
+                          overflow="auto"
+                          maxHeight={250}
+                          p={1.5}
+                        >
+                          {accepted.map((file, i) => (
+                            <Alert
+                              key={i}
+                              variant="outlined"
+                              severity="success"
+                              sx={{ mr: "2px", py: 0, px: 1 }}
+                            >
+                              <Typography
+                                whiteSpace="nowrap"
+                                overflow="hidden"
+                                textOverflow="ellipsis"
+                                variant="subtitle2"
+                              >
+                                {file.name}
+                              </Typography>
+                            </Alert>
+                          ))}
+                        </Box>
+                      </>
+                    ) : null}
+                  </Paper>
+                  <Paper>
+                    <Typography
+                      variant="subtitle2"
+                      p={1.5}
+                      color={rejected.length === 0 ? "textDisabled" : undefined}
+                    >
+                      Rejected ({rejected.length})
+                    </Typography>
+                    {rejected.length > 0 ? (
+                      <>
+                        <Divider />
+                        <Box
+                          display="flex"
+                          flexDirection="column"
+                          gap={1.5}
+                          overflow="auto"
+                          maxHeight={250}
+                          p={1.5}
+                        >
+                          {rejected.map((rejection, i) => (
+                            <Alert
+                              key={i}
+                              variant="outlined"
+                              severity="error"
+                              sx={{ mr: "2px", py: 0, px: 1 }}
+                            >
+                              <Typography
+                                whiteSpace="nowrap"
+                                overflow="hidden"
+                                textOverflow="ellipsis"
+                                variant="subtitle2"
+                              >
+                                {rejection.file.name}
+                              </Typography>
+                              <Typography variant="subtitle2" fontWeight={400}>
+                                {rejection.errors.at(0)?.message}
+                              </Typography>
+                            </Alert>
+                          ))}
+                        </Box>
+                      </>
+                    ) : null}
+                  </Paper>
+                </Box>
+              </FormControl>
+            );
+          }}
+        </form.AppField>
       </Box>
       <div>
         <Button type="submit" variant="contained" loading={form.state.isSubmitting}>
