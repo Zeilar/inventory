f00597fd5825947fc0885a228140d15cb19cd619
Zeilar <philip_angelin@hotmail.com>
2025-05-06 00:53:02 +0200

    fix: adjust skeletons to new design

--- app/items/loading.tsx
+++ app/items/loading.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { Box, Button, Card, CardActions, CardContent, Skeleton } from "@mui/material";
+import { Box, Button, Card, CardActions, CardContent, Checkbox, Skeleton } from "@mui/material";
 import { ItemsContainerLayout, ItemsHeaderLayout } from "./(components)";
 import { ItemSearchFieldLayout } from "@/features/item/components";
 import { useSearchParams } from "next/navigation";
 import { PER_PAGE } from "@/features/item/config";
 import { Link } from "@/components";
+import { Delete, Edit } from "@mui/icons-material";
 
 export function ItemsCardsSkeletons() {
   return (
@@ -15,16 +16,17 @@ export function ItemsCardsSkeletons() {
           key={i}
           sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}
         >
-          <CardContent sx={{ px: 1, py: 0, width: "100%" }}>
-            <Link href="#">
+          <CardContent sx={{ p: 1.5, width: "100%", display: "flex", alignItems: "center" }}>
+            <Checkbox sx={{ mr: 1.5 }} disabled />
+            <Link href="#" sx={{ width: "100%" }}>
               <Skeleton />
             </Link>
           </CardContent>
-          <CardActions sx={{ p: 0.75 }}>
-            <Button disabled color="primary">
+          <CardActions sx={{ p: 1.5, gap: 0.75 }}>
+            <Button variant="outlined" disabled startIcon={<Edit />} color="primary">
               Edit
             </Button>
-            <Button disabled color="error">
+            <Button variant="outlined" disabled startIcon={<Delete />} color="error">
               Delete
             </Button>
           </CardActions>
