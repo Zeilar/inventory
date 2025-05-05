d64e9df1c5718123597e6ed164579c1aac1fca41
Zeilar <philip_angelin@hotmail.com>
2025-05-06 00:30:10 +0200

    fix: make a single db call on create

--- features/item/components/create-form/action.ts
+++ features/item/components/create-form/action.ts
@@ -2,7 +2,7 @@
 
 import { db } from "@/features/db";
 import { itemsTable } from "@/features/db/schema";
-import { eq, type InferInsertModel } from "drizzle-orm";
+import type { InferInsertModel } from "drizzle-orm";
 import { existsSync } from "fs";
 import { mkdir, writeFile } from "fs/promises";
 import { revalidateTag } from "next/cache";
@@ -12,30 +12,27 @@ export async function createItem(
   data: InferInsertModel<typeof itemsTable>,
   files: File[]
 ): Promise<number> {
-  const { id } = db.insert(itemsTable).values(data).returning().get();
+  const { id } = db
+    .insert(itemsTable)
+    .values({ ...data, files: files.map((file) => file.name).join(",") })
+    .returning()
+    .get();
 
   const filesDir = resolve(process.cwd(), "files", `${id}`);
   if (!existsSync(filesDir)) {
     await mkdir(filesDir);
   }
 
-  const uploadedFiles: string[] = [];
   await Promise.all(
     files.map(async (file) => {
       const filePath = resolve(filesDir, file.name);
       try {
         await writeFile(filePath, Buffer.from(await file.arrayBuffer()));
-        uploadedFiles.push(file.name);
       } catch (error) {
         console.error(`Failed to write file ${file.name} to ${filePath}`, error);
       }
     })
   );
-
-  await db
-    .update(itemsTable)
-    .set({ files: uploadedFiles.join(","), ...data })
-    .where(eq(itemsTable.id, id));
 
   revalidateTag("items");
 
