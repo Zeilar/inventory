2b2b1e9e8dec8b9833a87600f692425f7a34f66b
Zeilar <philip_angelin@hotmail.com>
2025-06-16 06:04:44 +0200

    feat: restart containers when pulling

--- ci/setup.sh
+++ ci/setup.sh
@@ -7,15 +7,28 @@ if [ ! -d .git ]; then
   exit 1
 fi
 
-HOOK_SRC="ci/hooks/pre-push"
-HOOK_DEST=".git/hooks/pre-push"
+PRE_PUSH_HOOK_SRC="ci/hooks/pre-push"
+PRE_PUSH_HOOK_DEST=".git/hooks/pre-push"
 
-if [ ! -f "$HOOK_SRC" ]; then
-  echo "❌ Hook source '$HOOK_SRC' not found."
+if [ ! -f "$PRE_PUSH_HOOK_SRC" ]; then
+  echo "❌ Hook source '$PRE_PUSH_HOOK_SRC' not found."
   exit 1
 fi
 
-cp "$HOOK_SRC" "$HOOK_DEST"
-chmod +x "$HOOK_DEST"
+cp "$PRE_PUSH_HOOK_SRC" "$PRE_PUSH_HOOK_DEST"
+chmod +x "$PRE_PUSH_HOOK_DEST"
 
-echo "✅ Installed pre-push hook to $HOOK_DEST"
+echo "✅ Installed pre-push hook to $PRE_PUSH_HOOK_DEST"
+
+POST_MERGE_HOOK_SRC="ci/hooks/pre-push"
+POST_MERGE_HOOK_DEST=".git/hooks/pre-push"
+
+if [ ! -f "$POST_MERGE_HOOK_SRC" ]; then
+  echo "❌ Hook source '$POST_MERGE_HOOK_SRC' not found."
+  exit 1
+fi
+
+cp "$POST_MERGE_HOOK_SRC" "$POST_MERGE_HOOK_DEST"
+chmod +x "$POST_MERGE_HOOK_DEST"
+
+echo "✅ Installed post-merge hook to $POST_MERGE_HOOK_DEST"
